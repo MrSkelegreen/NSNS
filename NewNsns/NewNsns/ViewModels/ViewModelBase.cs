e881a056f4011cd5569cb4736c1df00d064cb55b
MrSkelegreen <104663261+MrSkelegreen@users.noreply.github.com>
2022-12-02 11:23:28 +0700

    NewNSNS

---
+++ NewNsns/NewNsns/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace NewNsns.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ NewNsns/NewNsns/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace NewNsns.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
