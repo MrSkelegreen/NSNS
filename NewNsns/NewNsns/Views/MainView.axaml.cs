f9c46f28eeb9bfefad477c803228a811af91e4a7
MrSkelegreen <104663261+MrSkelegreen@users.noreply.github.com>
2022-12-08 13:36:30 +0700

    testcom

--- NewNsns/NewNsns/Views/MainView.axaml.cs
+++ NewNsns/NewNsns/Views/MainView.axaml.cs
@@ -1,4 +1,10 @@
+using System.Net.Mail;
+using System.Reactive;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Media;
 
 namespace NewNsns.Views;
 
@@ -7,5 +13,24 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
+        this.NutrientsBtn.Background = Brushes.ForestGreen;
+    }
+
+    private void NutrientsMode_OnClick(object? sender, RoutedEventArgs e)
+    {
+        this.NutrientsBtn.Background = Brushes.ForestGreen;
+        this.RecipeBtn.Background = Brushes.LightGray;
+    }
+
+    private void RecipeBtn_OnClick(object? sender, RoutedEventArgs e)
+    {
+        this.RecipeBtn.Background = Brushes.ForestGreen;
+        this.NutrientsBtn.Background = Brushes.LightGray;
+    }
+
+
+    private void SettingsBtn_OnTapped(object? sender, TappedEventArgs e)
+    {
+        FlyoutBase.ShowAttachedFlyout(sender as Control);
     }
 }
